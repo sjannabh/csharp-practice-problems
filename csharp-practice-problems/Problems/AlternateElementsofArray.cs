34e87230a12376a71689386544353db62daa42b0
Srilekha Jannabhatla <srilekha.jannabhatla9@gmail.com>
2024-07-09 13:47:23 -0400

    Print alternate elements of an array

---
+++ csharp-practice-problems/Problems/AlternateElementsofArray.cs
@@ -0,0 +1,16 @@
+﻿namespace csharp_practice_problems.Problems
+{
+    public class AlternateElementsofArray
+    {
+        public void alternate(int[] arr)
+        {
+            for(int i = 0; i < arr.Length; i++)
+            {
+                if(i%2 == 0)
+                {
+                    Console.Write(arr[i]+ " ");
+                }
+            }
+        }
+    }
+}
