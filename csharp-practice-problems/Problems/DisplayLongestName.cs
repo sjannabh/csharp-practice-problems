08f28bc236accda6d0815d74ff18d023d0fc1aab
Srilekha Jannabhatla <srilekha.jannabhatla9@gmail.com>
2024-07-11 13:48:47 -0400

    Display longest name in the given array of strings

---
+++ csharp-practice-problems/Problems/DisplayLongestName.cs
@@ -0,0 +1,24 @@
+﻿namespace csharp_practice_problems.Problems
+{
+    public class DisplayLongestName
+    {
+        public void displayLong(string[] arr)
+        {
+            string res = "";
+            int n = arr.Length;
+            int len = 0;
+
+            for(int i = 0; i < n; i++)
+            {
+                if (arr[i].Length >= len)
+                {
+                    len = arr[i].Length;
+                    res = arr[i];
+                    Console.Write(arr[i] + " ");
+                }
+            }
+        }
+    }
+
+    
+}
